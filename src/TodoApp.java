4ad77fe303cb11de8cfb39ecf9c9786a432cbbdc
andreadev2025 <andrea.dev2025@gmail.com>
2025-09-02 18:15:19 +0200

    Prima versione ToDo Console App

---
+++ src/TodoApp.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TodoApp {
+    public static void main(String[] args) {
+        ArrayList<String> todos = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while(running) {
+            System.out.println("1) Aggiungi ToDo  2) Mostra ToDo  3) Esci");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // pulisce il buffer
+
+            switch(choice) {
+                case 1:
+                    System.out.print("Scrivi ToDo: ");
+                    String todo = scanner.nextLine();
+                    todos.add(todo);
+                    break;
+                case 2:
+                    System.out.println("Lista ToDo:");
+                    for(int i=0; i<todos.size(); i++) {
+                        System.out.println((i+1) + ") " + todos.get(i));
+                    }
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Scelta non valida!");
+            }
+        }
+
+        System.out.println("Bye!");
+        scanner.close();
+    }
+}
